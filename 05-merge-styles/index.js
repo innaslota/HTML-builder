dffa99aa48a33b749b525d2b644ec3fa1f793d0b
innaslota <innaslota8@gmail.com>
2023-05-07 17:31:26 +0300

    fix: remove synchronous function from code

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,22 +3,41 @@ const path = require('path');
 
 const stylesDir = path.join(__dirname, 'styles');
 const distDir = path.join(__dirname, 'project-dist');
-const bundlePath = path.join(distDir, 'bundle.css');
+const outputFile = path.join(distDir, 'bundle.css');
 
-const cssFiles = fs.readdirSync(stylesDir).filter(filename => {
-  const extension = path.extname(filename);
-  return extension === '.css';
+fs.readdir(stylesDir, (err, files) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const cssFiles = files.filter(file => path.extname(file) === '.css');
+
+  if (cssFiles.length === 0) {
+    console.log('No CSS files found in styles directory');
+    return;
+  }
+
+  const styles = [];
+
+  cssFiles.forEach((file, index) => {
+    fs.readFile(path.join(stylesDir, file), 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      styles[index] = data;
+
+      if (styles.filter(Boolean).length === cssFiles.length) {
+        fs.writeFile(outputFile, styles.join('\n'), err => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          console.log(`Styles merged into ${outputFile}`);
+        });
+      }
+    });
+  });
 });
-
-const styles = cssFiles.map(filename => {
-  const filePath = path.join(stylesDir, filename);
-  return fs.readFileSync(filePath, 'utf8');
-});
-
-const bundleContent = styles.join('\n');
-
-if (!fs.existsSync(distDir)) {
-  fs.mkdirSync(distDir);
-}
-
-fs.writeFileSync(bundlePath, bundleContent);
