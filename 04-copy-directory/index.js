5945527c674eeb313b3925c452b450d8c729ce57
innaslota <innaslota8@gmail.com>
2023-05-07 14:41:22 +0300

    feat: add solution to the task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const path = require('path');
 
+function copyDir(sourceDir, destinationDir) {
+  fs.mkdirSync(destinationDir, { recursive: true });
+
+  const files = fs.readdirSync(sourceDir);
+
+  files.forEach((file) => {
+    const sourceFile = path.join(sourceDir, file);
+    const destinationFile = path.join(destinationDir, file);
+    const fileStats = fs.statSync(sourceFile);
+    if (fileStats.isFile()) {
+      const fileContent = fs.readFileSync(sourceFile);
+      fs.writeFileSync(destinationFile, fileContent);
+    } else if (fileStats.isDirectory()) {
+      copyDir(sourceFile, destinationFile);
+    }
+  });
+}
+
+const sourceDir = path.resolve(__dirname, 'files');
+const destinationDir = path.resolve(__dirname, 'files-copy');
+
+copyDir(sourceDir, destinationDir);
+
+console.log(`The content of the folder ${sourceDir} is successfully copied into the folder ${destinationDir}`);
